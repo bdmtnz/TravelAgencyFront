e51c569655802bb878755db3f5e47586794fc4e9
bdmtnz <martinez_o@hotmail.es>
2023-12-19 01:24:10 -0500

    landing

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatIconModule,MatButtonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
