22d12f99b3da882e0ccc4016559298d211b61712
bdmtnz <martinez_o@hotmail.es>
2023-12-19 18:29:51 -0500

    login

--- src/app/pages/landing/components/login-modal/login-modal.component.ts
+++ src/app/pages/landing/components/login-modal/login-modal.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-modal',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatIconModule,MatButtonModule,MatInputModule,FormsModule,ReactiveFormsModule],
   templateUrl: './login-modal.component.html',
   styleUrl: './login-modal.component.scss'
 })
--- src/app/shared/components/info-modal/info-modal.component.ts
+++ src/app/shared/components/info-modal/info-modal.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-info-modal',
