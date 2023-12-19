322382ae637e7864bf9a67ce17070260e25047f4
bdmtnz <martinez_o@hotmail.es>
2023-12-18 23:50:09 -0500

    Advanced structure

---
+++ src/app/pages/layout/pages/hotel/components/manage-hotel-modal/manage-hotel-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-hotel-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-hotel-modal.component.html',
+  styleUrl: './manage-hotel-modal.component.scss'
+})
+export class ManageHotelModalComponent {
+
+}
