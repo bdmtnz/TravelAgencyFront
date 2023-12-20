cc65cd98e581ade6cd2576a7112d73c130d30622
bdmtnz <martinez_o@hotmail.es>
2023-12-20 16:37:57 -0500

    modal dialog login

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { LoginModalComponent } from './components/login-modal/login-modal.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MatToolbarModule,MatIconModule,MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatDialogModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
+
+  constructor(public dialog: MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(LoginModalComponent, {
+
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      
+    });
+  }
 
 }
