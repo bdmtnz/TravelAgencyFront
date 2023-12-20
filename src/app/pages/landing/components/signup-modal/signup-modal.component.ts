b7c4d2bc05e6fe5bdf6da77a6f2f401ef3b1212d
bdmtnz <martinez_o@hotmail.es>
2023-12-20 16:47:44 -0500

    create dialog register

--- src/app/pages/landing/components/signup-modal/signup-modal.component.ts
+++ src/app/pages/landing/components/signup-modal/signup-modal.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-signup-modal',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatIconModule,MatButtonModule,MatInputModule,FormsModule,ReactiveFormsModule],
   templateUrl: './signup-modal.component.html',
   styleUrl: './signup-modal.component.scss'
 })
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { LoginModalComponent } from './components/login-modal/login-modal.component';
+import { SignupModalComponent } from './components/signup-modal/signup-modal.component';
 
 @Component({
   selector: 'app-landing',
@@ -16,8 +17,18 @@ export class LandingComponent {
 
   constructor(public dialog: MatDialog) { }
 
-  openDialog(): void {
+  openDialogLogin(): void {
     const dialogRef = this.dialog.open(LoginModalComponent, {
+
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      
+    });
+  }
+
+  openDialogRegistrar(): void {
+    const dialogRef = this.dialog.open(SignupModalComponent, {
 
     });
     dialogRef.afterClosed().subscribe(result => {
