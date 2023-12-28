125a3e626a49304708d6d3bc9309abc63aac860c
bdmtnz <martinez_o@hotmail.es>
2023-12-28 17:59:40 -0500

    cal price

--- src/app/pages/layout/pages/room/component/manage-room-modal/manage-room-modal.component.ts
+++ src/app/pages/layout/pages/room/component/manage-room-modal/manage-room-modal.component.ts
@@ -39,7 +39,7 @@ export class ManageRoomModalComponent implements OnInit {
   accountingData!: FormGroup
   image!: FormGroup
   preview = 'https://img.freepik.com/foto-gratis/fondo_53876-32170.jpg?size=626&ext=jpg';
-
+  price:number = 0
   /**
    *
    */
@@ -57,26 +57,29 @@ export class ManageRoomModalComponent implements OnInit {
   ngOnInit(): void {
     this.previewImg()
     this.dataInitial()
+    this.accountingData.valueChanges.subscribe( value => {
+      this.calculatePrice(value)
+    })
   }
 
   builder() {
     this.basicData = this.formBuilder.group({
-      id:[''],
-      hotelId: ['',[Validators.required, Validators.minLength(1)]],
-      location: ['',[Validators.required]],
-      city: ['',[Validators.required]],
-      capacity: ['',[Validators.required]],
-      type: ['',[Validators.required,Validators.minLength(1)]],
+      id: [''],
+      hotelId: ['', [Validators.required, Validators.minLength(1)]],
+      location: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      capacity: ['', [Validators.required]],
+      type: ['', [Validators.required, Validators.minLength(1)]],
     });
 
     this.accountingData = this.formBuilder.group({
-      cost: [,[Validators.required]],
-      tax: [,[Validators.required]],
-      profit: [,[Validators.required]],
+      cost: [, [Validators.required]],
+      tax: [, [Validators.required]],
+      profit: [, [Validators.required, Validators.max(100), Validators.min(0)]],
       price: [],
     });
     this.image = this.formBuilder.group({
-      imageUrl: ['',[Validators.required]],
+      imageUrl: ['', [Validators.required]],
     });
   }
 
@@ -85,28 +88,38 @@ export class ManageRoomModalComponent implements OnInit {
       this.preview = value
     })
   }
-  dataInitial(){
-    this.basicData.controls['id'].setValue(this.data.data.id)
-    this.basicData.controls['hotelId'].setValue(this.data.data.hotelId)
-    this.basicData.controls['location'].setValue(this.data.data.location)
-    this.basicData.controls['city'].setValue(this.data.data.city)
-    this.basicData.controls['capacity'].setValue(this.data.data.capacity)
-    this.basicData.controls['type'].setValue(this.data.data.type)
-    this.accountingData.controls['cost'].setValue(this.data.data.cost)
-    this.accountingData.controls['tax'].setValue(this.data.data.tax)
-    this.accountingData.controls['profit'].setValue(this.data.data.profit)
-    this.accountingData.controls['price'].setValue(this.data.data.price)
-    this.image.controls['imageUrl'].setValue(this.data.data.imageUrl)
+  dataInitial() {
+    // this.basicData.controls['id'].setValue(this.data.data.id)
+    // this.basicData.controls['hotelId'].setValue(this.data.data.hotelId)
+    // this.basicData.controls['location'].setValue(this.data.data.location)
+    // this.basicData.controls['city'].setValue(this.data.data.city)
+    // this.basicData.controls['capacity'].setValue(this.data.data.capacity)
+    // this.basicData.controls['type'].setValue(this.data.data.type)
+    // this.accountingData.controls['cost'].setValue(this.data.data.cost)
+    // this.accountingData.controls['tax'].setValue(this.data.data.tax)
+    // this.accountingData.controls['profit'].setValue(this.data.data.profit)
+    // this.accountingData.controls['price'].setValue(this.data.data.price)
+    // this.image.controls['imageUrl'].setValue(this.data.data.imageUrl)
+    this.basicData.patchValue({ ...this.data.data })
+    this.accountingData.patchValue({ ...this.data.data })
+    this.image.patchValue({ ...this.data.data })
   }
 
 
+  calculatePrice(value: any) {
+    let valou1 = (( Math.floor(value.cost * value.tax )) / 100)
+    let valou2 = (( Math.floor(value.cost * value.profit )) / 100)
+    let priceTotal = Math.floor(value.cost + valou1 + valou2)
+    this.price = priceTotal
+    console.log(this.price)
+  }
   saveRoom() {
     // console.log("SAVE", this.data )
     this.basicData.markAllAsTouched()
     this.accountingData.markAllAsTouched()
     this.image.markAllAsTouched()
 
-    if (this.basicData.invalid || this.accountingData.invalid  || this.image.invalid) return
+    if (this.basicData.invalid || this.accountingData.invalid || this.image.invalid) return
     this.objetRoom = {
       ...this.basicData.value,
       ...this.accountingData.value,
