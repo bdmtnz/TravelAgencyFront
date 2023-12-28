bc6132f4f8f168e1625d9288c0dffecda937f6ed
bdmtnz <martinez_o@hotmail.es>
2023-12-27 20:06:58 -0500

    Advanced booking service

---
+++ src/app/shared/models/booking.model.ts
@@ -0,0 +1,93 @@
+export interface ITraveler {
+    id: string,
+    enabled: true,
+    person: {
+      id: string,
+      enabled: true,
+      contact: {
+        indicative: number,
+        value: string,
+        name: string
+      },
+      lastName: string,
+      birth: Date,
+      gender: number,
+      document: {
+        type: number,
+        value: string
+      },
+      email: string
+    },
+    credential: {
+      id: string,
+      enabled: true,
+      user: string,
+      rol: number,
+      password: string,
+      contact: {
+        indicative: number,
+        value: string,
+        name: string
+      }
+    },
+    bookings: Array<any>
+}
+
+export interface IContact {
+    indicative: number,
+    value: string,
+    name: string
+}
+
+export interface IPerson {
+    id: string,
+    enabled: true,
+    contact: IContact,
+    lastName: string,
+    birth: Date,
+    gender: number,
+    document: {
+      type: number,
+      value: string
+    },
+    email: string
+}
+
+export interface IHotel {
+    id: string,
+    enabled: true,
+    name: string,
+    description: string,
+    imageUrl: string,
+    minPrice: number
+}
+
+export interface IRoom {
+    id: string,
+    enabled: true,
+    hotel: IHotel,
+    location: string,
+    type: number,
+    cost: number,
+    tax: number,
+    profit: number,
+    capacity: number,
+    city: string,
+    imageUrl: string,
+    price: number,
+    bookings: Array<any>
+}
+
+export interface IBookingReponse {
+    id: string,
+    enabled: true,
+    roomId: string,
+    room: IRoom,
+    traveler: ITraveler,
+    guests: Array<IPerson>,
+    emergencyContact: IContact,
+    start: Date,
+    end: Date,
+    price: number,
+    quantityPeople: number
+}
