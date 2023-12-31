ca322a71abba1fe4d9819d86790a9c90c73c084e
bdmtnz <martinez_o@hotmail.es>
2023-12-31 16:29:03 -0500

    card state

---
+++ src/app/shared/components/card-statistics/card-statistics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-statistics',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-statistics.component.html',
+  styleUrl: './card-statistics.component.scss'
+})
+export class CardStatisticsComponent {
+
+}
