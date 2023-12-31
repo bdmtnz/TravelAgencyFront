66fa4a83716b832a797faf3898e08ea766f5b6e4
bdmtnz <martinez_o@hotmail.es>
2023-12-31 16:38:09 -0500

    card state

--- src/app/pages/layout/pages/home/home.component.ts
+++ src/app/pages/layout/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardStatisticsComponent } from '../../../../shared/components/card-statistics/card-statistics.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardStatisticsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
